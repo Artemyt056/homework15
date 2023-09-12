507ff44a66e30d8c3fced02d08154a3951d7ae28
Artemyt <artemyt056@gmail.com>
2023-09-13 00:27:40 +0300

    homeworke15

--- homework15.py
+++ homework15.py
@@ -2,8 +2,7 @@ import requests
 
 
 url = 'https://script.googleusercontent.com/macros/echo?user_content_key=NenZD9gakocbiOoGhH6OzCxZOBJJLDlGT4G3HQELaPzXjICPomz9dl25yBgvj6g3CeBVR0WxFyz_uNb9RZEf95arHsDy0nHym5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnLKny_gE5DMIdjlvbrWv9xoxuwxDqOpqCJs3UglsWjoAQ2lWu5fxfSVJyUVZburi0qSx8k1m6RR7rBLdTtHOSjM1uAHAYJN0Sdz9Jw9Md8uu&lib=MIzSw3Mi5stBFbGvsTZ7v23Ppx8Xgm0M2'
-
-
+git
 check_url = requests.get(url)
 
 checking_url = check_url.json()
