9614abce2eb4b413a7c5f497333257cad6a24433
Artemyt <artemyt056@gmail.com>
2023-09-13 00:24:04 +0300

    homework15

--- homework15.py
+++ homework15.py
@@ -1,11 +1,18 @@
 import requests
 
 
-url = 'https://script.googleusercontent.com/macros/echo?user_content_key=tl4jsL9vzu2dCxH7nFqvkCaA0fjdGnVpJnp0suJvN73Z1CQPc5bvjEH7lgPS5dNkA_UU6pEDyNEqIVP-vH1DSXKxx1CVpvk7m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnObB0kwUlVQh5Mo_4h0OkP9qmMhZMYpNiEqs0LzczH870hC4DVlpCU8BRi8qLLDAttZJlRufWV8M_OsAq0ThyCyqbM5bgh1mlNz9Jw9Md8uu&lib=MIzSw3Mi5stBFbGvsTZ7v23Ppx8Xgm0M2'
+url = 'https://script.googleusercontent.com/macros/echo?user_content_key=NenZD9gakocbiOoGhH6OzCxZOBJJLDlGT4G3HQELaPzXjICPomz9dl25yBgvj6g3CeBVR0WxFyz_uNb9RZEf95arHsDy0nHym5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnLKny_gE5DMIdjlvbrWv9xoxuwxDqOpqCJs3UglsWjoAQ2lWu5fxfSVJyUVZburi0qSx8k1m6RR7rBLdTtHOSjM1uAHAYJN0Sdz9Jw9Md8uu&lib=MIzSw3Mi5stBFbGvsTZ7v23Ppx8Xgm0M2'
 
 check_url = requests.get(url)
 
 checking_url = check_url.json()
 
-money = sum([person['money'] for person in checking_url['trip'] if not['gluten']])
-print(money)
+money = sum([person['money'] for person in checking_url['magazine']])
+money_remainder = sum([product['remainder'] for product in checking_url['magazine']])
+money_and_remainder = money * money_remainder
+print(money_and_remainder)
+
+money_not_gluten_ = sum([person['money'] for person in checking_url['magazine'] if not['gluten']])
+money_not_gluten_remainder = sum([product['remainder'] for product in checking_url['magazine']])
+money_not_gluten_and_remainder = money_not_gluten_ * money_not_gluten_remainder
+print(money_not_gluten_and_remainder)
